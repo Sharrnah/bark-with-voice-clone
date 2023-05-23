bca741533cd6a4c4217eb23ce43042f41f6553b5
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-05-23 21:03:33 +0200

    Add splitting for longer audio generation

--- bark/__init__.py
+++ bark/__init__.py
@@ -1,3 +1,4 @@
 from .api import generate_audio, text_to_semantic, semantic_to_waveform, save_as_prompt
 from .generation import SAMPLE_RATE, preload_models
 from .clonevoice import clone_voice
+from .text_processing import split_general_purpose, estimate_spoken_time
